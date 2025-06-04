25e3dc8cf85ae56acf4b3ffb424cfc9d83dd3495
TrishBatra18 <trish.idesign.market@gmail.com>
2025-06-05 01:36:47 +0530

    Updated service account handling and removed GOOGLE_APPLICATION_CREDENTIALS

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,8 @@
 import * as admin from "firebase-admin";
+import * as fs from "fs";
 
-// Load service account from ENV instead of file
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON!);
+// Load service account key
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS!, "utf-8"));
 
 if (!admin.apps.length) {
   admin.initializeApp({
